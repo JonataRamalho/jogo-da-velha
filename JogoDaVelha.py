d3804f1e223195fd3ae898fbd9cc368d0de3b704
Jonata Ramalho <jonataramalho.s@gmail.com>
2024-02-23 22:10:12 -0300

    feat: criar jogo da velha

---
+++ JogoDaVelha.py
@@ -0,0 +1,130 @@
+import copy
+from queue import Queue
+from SearchBfs import bfs
+from typing import List, Optional
+
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.player = 'X'
+
+    def print_board(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5)
+
+    def make_move(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.player
+            self.player = 'X' if self.player == 'O' else 'O'
+            return True
+        return False
+
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ':
+                return self.board[i][0]
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
+                return self.board[0][i]
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+            return self.board[0][0]
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+            return self.board[0][2]
+        return None
+
+    def is_full(self):
+        for row in self.board:
+            for cell in row:
+                if cell == ' ':
+                    return False
+        return True
+
+    def get_empty_cells(self):
+        empty_cells = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == ' ':
+                    empty_cells.append((i, j))
+        return empty_cells
+
+    def goal_test(self):
+        return self.check_winner() == "X"
+  
+    def successors(self, current_state):
+        successors_list = []
+
+        if not self.is_full():
+            empty_cells = self.get_empty_cells()
+            for i, j in empty_cells:
+                new_game = self
+                new_game.make_move(i, j)
+                successors_list.append(new_game)
+
+        return successors_list
+
+        
+def bfs_chat(game):
+    queue = Queue()
+    queue.put((game, []))
+
+    while not queue.empty():
+        node, path = queue.get()
+        if node.check_winner() == 'X':
+            return path
+        elif not node.is_full():
+            empty_cells = node.get_empty_cells()
+            for i, j in empty_cells:
+                new_game = copy.deepcopy(node)
+                new_game.make_move(i, j)
+                queue.put((new_game, path + [(i, j)]))
+        print('path', path)
+        print('queue', queue)
+    return None
+
+
+def main():
+    game = TicTacToe()
+    game.print_board()
+
+    while not game.is_full() and not game.check_winner():
+        row = int(input("Enter row (0, 1, 2): "))
+        col = int(input("Enter column (0, 1, 2): "))
+        
+        if not (0 <= row <= 2) or not (0 <= col <= 2):
+            print("Invalid input. Please enter numbers between 0 and 2.")
+            continue
+        
+        if not game.make_move(row, col):
+            print("Invalid move. Try again.")
+            continue
+        
+        game.print_board()
+        
+        if game.check_winner():
+            print("You won!")
+            break
+        
+        if game.is_full():
+            print("It's a draw!")
+            break
+        
+        print("Computer's turn:")
+
+        bfs_path = bfs(game, game.goal_test, game.successors)
+        # bfs_path = bfs_chat(game)
+
+
+        if bfs_path:
+            print(bfs_path)
+            for move in bfs_path:
+                game.make_move(*move)
+                game.print_board()
+                print()
+                break
+        else:
+            print("No winning move found for the computer.")
+            break
+
+if __name__ == "__main__":
+    main()
